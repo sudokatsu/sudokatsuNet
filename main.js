a2fcb7058a61dd20104e289e3ece5d5e0d59d4d8
sudokatsu <kurtisrios@gmail.com>
2025-02-24 19:04:48 -0800

    Audio working

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-function playAudio() {
-  const sound = document.getElementById("select");
+let sound = document.getElementById('selectSound');
+let navOptions = document.getElementsByClassName('menuOptions');
+
+function controlAudioNav(event) {
   sound.play();
+  event.preventDefault(); //will stop the link href to call the blog page
+
+  setTimeout(function () {
+    window.location.href = '/pages/about.html'; 
+  }, 750);
 }
+
+Array.from(navOptions).forEach((option) => {
+  option.addEventListener('click', controlAudioNav);
+});
