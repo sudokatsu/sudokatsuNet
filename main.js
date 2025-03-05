3b0023818b5179de0ea26dcdfc829961c46bd3ae
sudokatsu <kurtisrios@gmail.com>
2025-03-05 15:34:22 -0800

    Fix navigation between all pages, add Resume file

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let sound = document.getElementById('selectSound');
 let homeContainer = document.getElementById('homeContainer');
-let backArrow = document.getElementById('backArrow');
+let backArrows = document.getElementsByClassName('backArrow');
 let menuOptionsContainer = document.getElementById('menuOptionsContainer');
 let navOptions = document.getElementsByClassName('menuOptions');
 let transitionBars = document.getElementsByClassName('transitionAnimation');
@@ -24,10 +24,7 @@ birth.addEventListener('ended', () => {
   loop.setAttribute('loop', 'true');
 });
 
-// Plays audio, css transition, and displays new content
-function controlAudioNav() {
-  sound.play();
-
+function pageTransition() {
   barsArray.forEach((bar) => {
     bar.style.transform = 'translateX(0)';
   });
@@ -36,22 +33,33 @@ function controlAudioNav() {
       bar.style.transform = 'translateX(-100vw)';
     });
   }, 2100);
-
-  if (this.id === 'backArrow') {
-    setTimeout(function () {
-      homeContainer.style.display = 'flex';
-    }, 2000);
-  } else {
-    setTimeout(function () {
-      document
-        .getElementById('backArrow')
-        .addEventListener('click', controlAudioNav);
-      homeContainer.style.display = 'none';
-      this.style.display = 'flex';
-      this.style.flex = '1';
-    }, 2000);
-  }
 }
+
+function controlBackPress() {
+  pageTransition();
+  setTimeout(function () {
+    homeContainer.style.display = 'flex';
+  }, 2000);
+  sound.play();
+}
+
+// Plays audio, css transition, and displays new content
+function controlAudioNav() {
+  const selected = document.getElementById(this.innerHTML);
+
+  sound.play();
+  pageTransition();
+
+  setTimeout(function () {
+    homeContainer.style.display = 'none';
+    selected.style.display = 'flex';
+    selected.style.flex = '1';
+  }, 2000);
+}
+
+Array.from(backArrows).forEach((arrow) => {
+  arrow.addEventListener('click', controlBackPress);
+});
 
 Array.from(navOptions).forEach((option) => {
   option.addEventListener('click', controlAudioNav);
